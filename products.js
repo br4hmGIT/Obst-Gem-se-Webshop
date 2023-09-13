c4161e192694ceaf94a2e923580bc3833473b8c5
br4hm <haehnlein.alexander@gmx.de>
2023-09-13 21:02:26 +0200

    First Commit

---
+++ products.js
@@ -0,0 +1,11 @@
+const products = [
+    { id: 1, name: "Kirsche", price: 4.99 },
+    { id: 2, name: "Wildbeeren-Mischung", price: 6.75 },
+    { id: 3, name: "Heidelbeere", price: 5.25 },
+    { id: 4, name: "Limette", price: 3.49 },
+    { id: 5, name: "Orange", price: 2.99 },
+    { id: 6, name: "Pfirsich", price: 3.95 },
+    { id: 7, name: "Birne", price: 2.85 },
+    { id: 8, name: "Erdbeere", price: 7.25 }
+  ]
+

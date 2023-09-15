0e1be2f6fc55a43603e7ed39a2633b1fae3619fa
br4hm <haehnlein.alexander@gmx.de>
2023-09-15 13:12:30 +0200

    Produkt-Array aktualisiert (img source hinzugefügt)

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log(products[1]);
+// select elements
+
+const productsEl = document.querySelector('.product-container');
+
+
+// Render Products
+
+function renderProducts(){
+    products.forEach((product) => {
+        productsEl.innerHTML = `
+        <div class="product">
+                <img src="./src/img/cherry.jpg" alt="Produkt 1">
+                <h2>Kirsche</h2>
+                <p>Eine wirkliche schmackhafte Kirsche macht doch jeden glücklich.</p>
+                <div class="button-wrapper">
+                    <button>Hinzufügen</button>
+                </div>
+            </div>
+        
+        `
+
+    })
+}
--- products.js
+++ products.js
@@ -1,11 +1,51 @@
 const products = [
-    { id: 1, name: "Kirsche", price: 4.99 },
-    { id: 2, name: "Wildbeeren-Mischung", price: 6.75 },
-    { id: 3, name: "Heidelbeere", price: 5.25 },
-    { id: 4, name: "Limette", price: 3.49 },
-    { id: 5, name: "Orange", price: 2.99 },
-    { id: 6, name: "Pfirsich", price: 3.95 },
-    { id: 7, name: "Birne", price: 2.85 },
-    { id: 8, name: "Erdbeere", price: 7.25 }
+    { 
+      id: 1, 
+      name: "Kirsche", 
+      price: 4.99,
+      imgSrc: "./src/img/cherry.jpg" 
+    },
+    { 
+      id: 2, 
+      name: "Wildbeeren-Mischung", 
+      price: 6.75, 
+      imgSrc: "./src/img/berry-mix.jpg" 
+    },
+    { 
+      id: 3, 
+      name: "Heidelbeere", 
+      price: 5.25,
+      imgSrc: "./src/img/blueberry.jpg" 
+    },
+    { 
+      id: 4, 
+      name: "Limette", 
+      price: 3.49,
+      imgSrc: "./src/img/lime.jpg" 
+    },
+    { 
+      id: 5, 
+      name: "Orange", 
+      price: 2.99,
+      imgSrc: "./src/img/orange.jpg" 
+    },
+    { 
+      id: 6, 
+      name: "Pfirsich", 
+      price: 3.95,
+      imgSrc: "./src/img/peach.jpg" 
+    },
+    { 
+      id: 7, 
+      name: "Birne", 
+      price: 2.85,
+      imgSrc: "./src/img/pear.jpg"  
+    },
+    { 
+      id: 8,
+      name: "Erdbeere", 
+      price: 7.25,
+      imgSrc: "./src/img/strawberry.jpg" 
+    }
   ]
 

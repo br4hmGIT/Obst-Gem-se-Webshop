b2aa5fa35c1e58ca6df24e6c9b4753842b408fde
br4hm <haehnlein.alexander@gmx.de>
2023-09-15 13:22:42 +0200

    description hinzugefügt

--- products.js
+++ products.js
@@ -3,49 +3,57 @@ const products = [
       id: 1, 
       name: "Kirsche", 
       price: 4.99,
-      imgSrc: "./src/img/cherry.jpg" 
+      imgSrc: "./src/img/cherry.jpg",
+      description: "Eine wirkliche schmackhafte Kirsche macht doch jeden glücklich." 
     },
     { 
       id: 2, 
       name: "Wildbeeren-Mischung", 
       price: 6.75, 
-      imgSrc: "./src/img/berry-mix.jpg" 
+      imgSrc: "./src/img/berry-mix.jpg",
+      description: "Ich kann mich nicht entscheiden. Ach, was soll's. Ich nehme einfach alles!"  
     },
     { 
       id: 3, 
       name: "Heidelbeere", 
       price: 5.25,
-      imgSrc: "./src/img/blueberry.jpg" 
+      imgSrc: "./src/img/blueberry.jpg",
+      description: "Blaue Beeren? Blaue Beeren!"  
     },
     { 
       id: 4, 
       name: "Limette", 
       price: 3.49,
-      imgSrc: "./src/img/lime.jpg" 
+      imgSrc: "./src/img/lime.jpg",
+      description: "Es ist heiß, man schwitzt, man braucht Erfrischung. Limetten erfrischen!"  
     },
     { 
       id: 5, 
       name: "Orange", 
       price: 2.99,
-      imgSrc: "./src/img/orange.jpg" 
+      imgSrc: "./src/img/orange.jpg",
+      description: "Muss nicht unbedingt Weihnachtszeit sein, damit die schmecken. Orangen wusste schon Don Corleone zu schätzen."
     },
     { 
       id: 6, 
       name: "Pfirsich", 
       price: 3.95,
-      imgSrc: "./src/img/peach.jpg" 
+      imgSrc: "./src/img/peach.jpg",
+      description: "Ein Obst perfekt für den Sommer gefällig?. Bitteschön, Pfirsich!"  
     },
     { 
       id: 7, 
       name: "Birne", 
       price: 2.85,
-      imgSrc: "./src/img/pear.jpg"  
+      imgSrc: "./src/img/pear.jpg",
+      description: "Birnen sind gut für unsere Birnen."   
     },
     { 
       id: 8,
       name: "Erdbeere", 
       price: 7.25,
-      imgSrc: "./src/img/strawberry.jpg" 
+      imgSrc: "./src/img/strawberry.jpg",
+      description: "Mit Schlagsahne, auf einer Torte oder püriert - Erdbeeren sind immer schmackofatzo"  
     }
   ]
 
